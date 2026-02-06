2c07f06e62dd4be47f243f38dd2f0cef4457220f
kingsleylam-0201 <767020931@qq.com>
2026-02-06 14:53:28 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 });
